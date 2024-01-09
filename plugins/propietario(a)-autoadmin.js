00d63cf2f611526e11258649ab1cd4ba072d3049
EdwinJVG77 <145831762+EdwinJVG77@users.noreply.github.com>
2024-01-09 02:33:07 -0500

    Delete plugins directory

--- plugins/propietario(a)-autoadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `*𝗬𝗔 𝗘𝗦 𝗔𝗗𝗠𝗜𝗡 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 𝗠𝗜 𝗣𝗥𝗢𝗣𝗜𝗘𝗧𝗔𝗥𝗜𝗢(𝗔)*`
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^admin.|atad|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
